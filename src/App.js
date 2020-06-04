7da396f280f74920e4fe19f84a034e9f2d161a0d
Abbas Mokhtari <49838027+abbasmokhtari@users.noreply.github.com>
2020-06-04 12:57:00 +0100

    React Router installed
    command is
    npm i react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Navbar from './Layout/Navbar'
 
 function App() {
   return (
     <div className="App">
-     <h1>Hello</h1>
+      <Navbar />
     </div>
   );
 }
